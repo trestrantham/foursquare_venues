fbdcba31f70be7a51a89d00c628b9270306f10e1
Tres Trantham <tres@trestrantham.com>
2011-06-22 00:31:15 -0400

    fixed icon

--- lib/foursquare_venues/venue.rb
+++ lib/foursquare_venues/venue.rb
@@ -59,7 +59,8 @@ module FoursquareVenues
     # return the url to the icon of the primary category
     # if no primary is available, then return a default icon
     def icon
-			primary_category ? primary_category.icon : "https://foursquare.com/img/categories/none.png"
+			#primary_category ? primary_category.icon : "https://foursquare.com/img/categories/none.png"
+			"https://foursquare.com/img/categories/none.png"
     end
     
     def short_url
